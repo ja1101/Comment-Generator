b4fbd869475fda3a54ddb2425f99ea6dc9112889
iberiaz9 <alvarado.ja53@gmail.com>
2021-04-04 14:02:56 -0700

    Update run.py
    Added session secret key.

--- run.py
+++ run.py
@@ -2,4 +2,6 @@
 from comment_generator import app
  
 if __name__ == "__main__":
-  app.run()
+    # The session object needs a secret key
+    app.secret_key = "HopKIdf78/*9*PO72xQ89Fg??"
+    app.run()
